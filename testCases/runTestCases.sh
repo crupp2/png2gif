0467184fcdb13a79faabd42aabc03f43a62b9a95
crupp2 <17787535+crupp2@users.noreply.github.com>
2019-03-05 20:42:34 -0700

    Added correct file1b.gif external reference file. Added test case script. Added test case correctly processed reference files.

---
+++ testCases/runTestCases.sh
@@ -0,0 +1,8 @@
+# Convert single png frame to a single gif frame
+../png2gif file1b.png
+../png2gif file1c.png
+../png2gif file1d.png
+../png2gif file1e.png
+../png2gif file1f.png
+../png2gif file1g.gif file1g.png
+
