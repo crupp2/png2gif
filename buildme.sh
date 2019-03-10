a402abf25f1216e753cf1c990ba83eec4344d193
crupp2 <17787535+crupp2@users.noreply.github.com>
2019-03-09 20:51:34 -0700

    Added test case for creating a movie. It works, but resulting file is large and animation is slow because it is rewriting the entire frame rather than making use of transparent pixels or redrawing smaller regions. Also changed compile flag to use code optimizations (i.e., from -g to -O3).

--- buildme.sh
+++ buildme.sh
@@ -1,16 +1,19 @@
-clang++ -g -c -o libLZWlib.o LZWlib.cpp
+#export CFLAGS=-g
+export CFLAGS=-O3
 
-clang -g -c -o pngReader.o pngReader.c
+clang++ $CFLAGS -c -o libLZWlib.o LZWlib.cpp
 
-clang -g -c -o gifWriter.o gifWriter.c
+clang $CFLAGS -c -o pngReader.o pngReader.c
 
-clang -g -c -o png2gif.o png2gif.c
+clang $CFLAGS -c -o gifWriter.o gifWriter.c
 
-clang -g -c -o medianCut.o medianCut.c
+clang $CFLAGS -c -o png2gif.o png2gif.c
 
-clang -g -c -o dither.o dither.c
+clang $CFLAGS -c -o medianCut.o medianCut.c
 
-clang++ -o png2gif png2gif.o pngReader.o gifWriter.o medianCut.o dither.o libLZWlib.o -lz
+clang $CFLAGS -c -o dither.o dither.c
+
+clang++ $CFLAGS -o png2gif png2gif.o pngReader.o gifWriter.o medianCut.o dither.o libLZWlib.o -lz
 
 clang -g -c -o LZWcompressTest.o LZWcompressTest.c
 clang++ -o lzwtest LZWcompressTest.o libLZWlib.o -lz
--- testCases/runTestCases.sh
+++ testCases/runTestCases.sh
@@ -11,3 +11,8 @@
 
 # Test ncolorbits and forcebw
 ../png2gif file1f_full_dither.gif -n 1 file1f_full.png -d -f
+
+# Test animation
+cd movie
+../../png2gif -t 0.05 file1a.gif file1a_f-01.png file1a_f-02.png file1a_f-03.png file1a_f-04.png file1a_f-05.png file1a_f-06.png file1a_f-07.png file1a_f-08.png file1a_f-09.png file1a_f-10.png file1a_f-11.png file1a_f-12.png file1a_f-13.png file1a_f-14.png file1a_f-15.png file1a_f-16.png file1a_f-17.png file1a_f-18.png file1a_f-19.png file1a_f-20.png file1a_f-21.png file1a_f-22.png file1a_f-23.png file1a_f-24.png file1a_f-25.png file1a_f-26.png file1a_f-27.png file1a_f-28.png file1a_f-29.png file1a_f-30.png file1a_f-31.png file1a_f-32.png file1a_f-33.png file1a_f-34.png file1a_f-35.png file1a_f-36.png
+cd ..
